f0c16aafd520b241da3f7fca70aee016de852977
Pratik Kargathra <pratikkargathra.work@gmail.com>
2025-02-17 23:20:06 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -1,11 +1,10 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Sanitize inputs
     $name = strip_tags(trim($_POST["name"]));
     $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
     $message = trim($_POST["message"]);
 
-    if ( empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+    if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
         http_response_code(400);
         echo "Please complete the form correctly.";
         exit;
@@ -13,10 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $recipient = "pratikkargathra.work@gmail.com";
     $subject = "New contact from $name";
-    $email_content = "Name: $name\n";
-    $email_content .= "Email: $email\n\n";
-    $email_content .= "Message:\n$message\n";
-
+    $email_content = "Name: $name\nEmail: $email\n\nMessage:\n$message\n";
     $email_headers = "From: $name <$email>";
 
     if (mail($recipient, $subject, $email_content, $email_headers)) {
